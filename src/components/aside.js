7aede5a5d0efcdd063e8929489ed8392851b6d44
Vishal Gupta <ims.vishal@gmail.com>
2021-04-20 13:26:52 +0530

    Updated tax change methods

--- src/components/aside.js
+++ src/components/aside.js
@@ -1,18 +1,24 @@
-const Aside = ({ tax, updateTax }) => (
-  <div className="bg-light pt-aside p-1 border">
-    <section>
-      <label>Tax</label>
-      <input
-        type="text"
-        value={tax}
-        onChange={(e) => updateTax(e.target.value)}
-      />
-      <span>%</span>
-    </section>
-    <section>
-      <button className="btn btn-sm btn-block btn-secondary">Print</button>
-    </section>
-  </div>
-);
+const Aside = ({ tax, updateTax }) => {
+  const update = (e) => {
+    const tax = isNaN(parseInt(e.target.value))
+      ? 0
+      : parseFloat(e.target.value);
+    console.log(tax);
+    updateTax(tax);
+  };
+
+  return (
+    <div className="bg-light pt-aside p-1 border">
+      <section>
+        <label>Tax</label>
+        <input type="text" value={tax} onChange={update} />
+        <span>%</span>
+      </section>
+      <section>
+        <button className="btn btn-sm btn-block btn-secondary">Print</button>
+      </section>
+    </div>
+  );
+};
 
 export default Aside;
