13fb651df0b29279123a74974e4e9d7b84be63e5
Vishal Gupta <ims.vishal@gmail.com>
2021-04-22 14:06:57 +0530

    Styling updates

--- src/components/products.js
+++ src/components/products.js
@@ -2,12 +2,12 @@ const Products = ({ items, tax }) => (
   <div>
     <table className="pt-products">
       <thead>
-        <tr className="bg-secondary text-light">
+        <tr className="bg-secondary text-light text-center">
           <th>#</th>
           <th>Item / Description</th>
-          <th className="text-right">Rate</th>
-          <th className="text-center">Quantity</th>
-          <th className="text-right">Price</th>
+          <th>Rate</th>
+          <th>Quantity</th>
+          <th>Price</th>
         </tr>
       </thead>
       <tbody>
@@ -16,21 +16,27 @@ const Products = ({ items, tax }) => (
 
           return (
             <tr key={i}>
-              <td className="text-center border-right">
-                {i + 1}&#160;
-                <button className="btn btn-sm btn-light pt-no-print">
-                  &#10060;
-                </button>
-              </td>
+              <td className="text-center border-right">{i + 1}&#160;</td>
               <td className="text-left border-right">
-                &#8377;&#160;{item.description}
+                <input
+                  type="text"
+                  value={item.description}
+                  onChange={() => {}}
+                />
               </td>
               <td className="text-right border-right">
-                {item.price.toFixed(2)}
+                &#8377;&#160;{item.price.toFixed(2)}
               </td>
               <td className="text-center border-right">{item.quantity}</td>
               <td className="text-right">
                 &#8377;&#160;{(item.price * item.quantity).toFixed(2)}
+                <button
+                  className="btn btn-sm btn-light pt-no-print"
+                  title="Delete this item"
+                  onClick={() => {}}
+                >
+                  &#10060;
+                </button>
               </td>
             </tr>
           );
